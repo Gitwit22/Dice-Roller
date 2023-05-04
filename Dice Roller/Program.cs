c808a27409460ab43a436e9f3cd0dac29887c6dc
John Steele <124938266+Gitwit22@users.noreply.github.com>
2023-05-04 18:15:16 -0500

    Update Program.cs

--- Dice Roller/Program.cs
+++ Dice Roller/Program.cs
@@ -1,16 +1,9 @@
 ﻿
 Console.WriteLine("Welcome to the craps program!");
-Console.WriteLine("How many sides should each die have?");
-int sides = int.Parse(Console.ReadLine());
-
 
 //Initialized variables and random number generator
 
 
-Random ran = new Random();
-int diceRoll = ran.Next(1, sides);
-int diceRoll2 = ran.Next(1, sides);
-int diceTotal = (diceRoll + diceRoll2);
 bool playAgain = true;
 
 
@@ -19,13 +12,22 @@ bool playAgain = true;
 
 while (playAgain = true)
 {
+    Console.WriteLine("How many sides should each die have?");
+
+    int sides = int.Parse(Console.ReadLine());
+    Random ran = new Random();
+    int diceRoll = ran.Next(1, sides);
+    int diceRoll2 = ran.Next(1, sides);
+    int diceTotal = (diceRoll + diceRoll2);
+    
+
 
 
     //re - Initialized variables and random number generator for while loop
 
-     diceRoll = ran.Next(1, sides);
-     diceRoll2 = ran.Next(1, sides);
-     diceTotal = (diceRoll + diceRoll2);
+    //diceRoll = ran.Next(1, sides);
+    // diceRoll2 = ran.Next(1, sides);
+    // diceTotal = (diceRoll + diceRoll2);
 
 
     //Program functions/methods
